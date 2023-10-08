d2935882987426729134661fd89fbaff7815fe4a
journeystart192023 <journeystart192023@gmail.com>
2023-10-08 17:37:35 +0530

    update

--- todo_list/src/Login.jsx
+++ todo_list/src/Login.jsx
@@ -1,37 +1,57 @@
 import React, { useState } from "react";
 import LogoutButton from './LogoutButton';
-export const Login = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-  
-    const [isLoggedIn, setIsLoggedIn] = useState(false); // Add a state variable for login status
+import Notes from "./Notes";
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        alert('Login Successfully');
-        console.log(email);
-        setIsLoggedIn(true); 
+export const Login = (props) => {
+  const [form, setForm] = useState({});
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleForm = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8080/login", {
+      method: 'POST',
+      body: JSON.stringify(form),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    let result = response.status;
+    if (Number(result) === 200) {
+      setIsLoggedIn(true);
+      alert('Login Successfully');
+    } else {
+      alert('Please Enter a valid email and password');
     }
-    const handleLogout = () => {
-      setIsLoggedIn(false); // Set login status to false when the user logs out
-    }
-    return (
-      <div className="auth-form-container">
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div className="auth-form-container">
       {isLoggedIn ? (
         <div>
           <h2>Welcome, User</h2>
+          <Notes />
           <LogoutButton onLogout={handleLogout} />
         </div>
       ) : (
         <form className="login-form" onSubmit={handleSubmit}>
           <label htmlFor="email">email</label>
-          <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter your email address" id="email" name="email" />
+          <input onChange={handleForm} type="email" placeholder="Enter your email address" id="email" name="email" />
           <label htmlFor="password">password</label>
-          <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Enter your password" id="password" name="password" />
+          <input onChange={handleForm} type="password" placeholder="Enter your password" id="password" name="password" />
           <button type="submit">Log In</button>
         </form>
       )}
       <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
     </div>
-    )
-}
+  );
+};
--- todo_list/src/Notes.jsx
+++ todo_list/src/Notes.jsx
@@ -1,24 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import './notes.css'
 function Notes() {
-  // Initialize state for managing notes
   const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState({ title: '', body: '', color: '' });
+  const [form, setForm] = useState({
+    title: "",
+    body: "",
+    color: "#ffffff"
+  });
 
-  // Function to add a new note
-  const addNote = () => {
-    if (newNote.title && newNote.body && newNote.color) {
-      setNotes([...notes, newNote]);
-      setNewNote({ title: '', body: '', color: '' });
+  const addNote = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:8080/createPost", {
+        method: "POST",
+        body: JSON.stringify(form),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
+      if (response.status) {
+        const createdNote = await response.json();
+        setNotes([...notes, createdNote]);
+        setForm({
+          title: "",
+          body: "",
+          color: "#ffffff"
+        });
+      } else {
+        console.error("Error creating note");
+        // Handle error state or show error message to the user
+      }
+    } catch (error) {
+      console.error("Error creating note:", error);
+      // Handle error state or show error message to the user
     }
   };
 
   // Function to edit a note
   const editNote = (index) => {
-    // Implement your edit logic here, you can open a modal or provide input fields for editing
-    // For simplicity, we'll just remove the note for this example
-    const updatedNotes = [...notes];
-    updatedNotes.splice(index, 1);
-    setNotes(updatedNotes);
+    // Implement your edit logic here
+    // For example, open a modal or provide input fields for editing
+    // Update the note in the notes state after editing
+    // const updatedNotes = [...notes];
+    // updatedNotes[index] = updatedNoteData;
+    // setNotes(updatedNotes);
   };
 
   return (
@@ -27,27 +53,36 @@ function Notes() {
       <div className="notes-container">
         <div className="new-note">
           <h3>Create a New Note</h3>
-          <input
-            type="text"
-            placeholder="Title"
-            value={newNote.title}
-            onChange={(e) => setNewNote({ ...newNote, title: e.target.value })}
-          />
-          <textarea
-            placeholder="Note"
-            value={newNote.body}
-            onChange={(e) => setNewNote({ ...newNote, body: e.target.value })}
-          />
-          <input
-            type="color"
-            value={newNote.color}
-            onChange={(e) => setNewNote({ ...newNote, color: e.target.value })}
-          />
-          <button onClick={addNote}>Add Note</button>
+          <form onSubmit={addNote}>
+            <input
+              type="text"
+              placeholder="Title"
+              name="title"
+              value={form.title}
+              onChange={(e) => setForm({ ...form, title: e.target.value })}
+            />
+            <textarea
+              placeholder="Note"
+              name="body"
+              value={form.body}
+              onChange={(e) => setForm({ ...form, body: e.target.value })}
+            />
+            <input
+              type="color"
+              name="color"
+              value={form.color}
+              onChange={(e) => setForm({ ...form, color: e.target.value })}
+            />
+            <button type="submit">Add Note</button>
+          </form>
         </div>
         <div className="all-notes">
           {notes.map((note, index) => (
-            <div key={index} className="note" style={{ backgroundColor: note.color }}>
+            <div
+              key={index}
+              className="note"
+              style={{ backgroundColor: note.color }}
+            >
               <h3>{note.title}</h3>
               <p>{note.body}</p>
               <button onClick={() => editNote(index)}>Edit</button>
