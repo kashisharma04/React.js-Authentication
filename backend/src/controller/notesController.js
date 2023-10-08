c281cb3203fca991867925a7c6c68e624b0b9ed1
kashi <kashisharmaujjn@gmail.com>
2023-10-08 22:06:52 +0530

    backend code

--- backend/src/controller/notesController.js
+++ backend/src/controller/notesController.js
@@ -20,7 +20,41 @@ const createNote = async function (req, res) {
     return res.status(500).send({ status: false, message: err.message });
   }
 };
+const getNote = async function(req,res){
+  try {
+    const myNote = await noteModel.find()
+    res.status(200).json({ status: true, myNote: myNote })
+} catch (error) {
+    console.log(error)
+    res.status(500).json({ status: false, error: error })
+}
+}
+const updateNote = async function (req,res){
+  try {
+    const { id } = req.params; // Assuming you use a route parameter to identify the note to update
+    const { title, body, color } = req.body;
+
+    // Checking if the note with the given ID exists
+    const existingNote = await noteModel.findById(id);
+
+    if (!existingNote) {
+      return res.status(404).send({ status: false, message: 'Note not found' });
+    }
+
+    // Update the note with the new data
+    existingNote.title = title;
+    existingNote.body = body;
+    existingNote.color = color;
+
+    // Save the updated note
+    const updatedNote = await existingNote.save();
+
+    return res.status(200).send({ status: true, data: updatedNote });
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.message });
+  }
+};
 
 module.exports = {
-  createNote
+  createNote, updateNote, getNote
 };
--- backend/src/route/route.js
+++ backend/src/route/route.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { login,createUser,getUser } = require('../controller/userController');
-const {  createNote} = require('../controller/notesController')
+const {  updateNote, getNote,createNote} = require('../controller/notesController')
 const { authentication , authorization } = require('../middleware/middleware')
 
 router.get('/', (req, res) => {
@@ -12,5 +12,7 @@ router.get('/', (req, res) => {
 router.post("/registration", createUser)
 router.get("/getuser", getUser)
 router.post('/login', login)
-router.post('/createPost',createNote)
+router.post('/createPost',createNote);
+router.put('/updateNote/:id',updateNote);
+router.get('/getNote',getNote);
 module.exports = router;
