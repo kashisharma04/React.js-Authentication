75103ddcc2226973d4a2ba602069be69f5ffeae2
kashi <kashisharmaujjn@gmail.com>
2023-10-09 11:05:30 +0530

    frontend Project

--- todo_list/src/Notes.jsx
+++ todo_list/src/Notes.jsx
@@ -16,19 +16,19 @@ function Notes() {
   }, []);
 
   // Frontend GET API
-  const fetchNotes = async () => {
-    try {
-      const response = await fetch("http://localhost:8080/getNote"); // Use "/getNote" instead of "/getNotes"
-      if (response.status === 200) { // Check for a specific status code (e.g., 200 for success)
-        const data = await response.json();
-        setNotes(data.myNote); // Use data.myNote to access the notes
-      } else {
-        console.error("Error fetching notes");
-      }
-    } catch (error) {
-      console.error("Error fetching notes:", error);
+const fetchNotes = async () => {
+  try {
+    const response = await fetch("http://localhost:8080/getNote"); // Use "/getNote" instead of "/getNotes"
+    if (response.status === 200) { // Check for a specific status code (e.g., 200 for success)
+      const data = await response.json();
+      setNotes(data.myNote); // Use data.myNote to access the notes
+    } else {
+      console.error("Error fetching notes");
     }
-  };
+  } catch (error) {
+    console.error("Error fetching notes:", error);
+  }
+};
 
 
   const addNote = async (e) => {
@@ -105,74 +105,74 @@ function Notes() {
 
   return (
     <>
-      <div className="notes-app">
-        <h2>Notes</h2>
-        <div className="notes-container">
-          <div className="new-note">
-            <h3>Create a New Note</h3>
-            <form onSubmit={addNote}>
-              <input
-                type="text"
-                placeholder="Title"
-                name="title"
-                value={form.title}
-                onChange={(e) => setForm({ ...form, title: e.target.value })}
-              />
-              <textarea
-                placeholder="Note"
-                name="body"
-                value={form.body}
-                onChange={(e) => setForm({ ...form, body: e.target.value })}
-              />
-              <input
-                type="color"
-                name="color"
-                value={form.color}
-                onChange={(e) => setForm({ ...form, color: e.target.value })}
-              />
-              <button type="submit">Add Note</button>
-            </form>
-          </div>
-          <div className="all-notes">
-            {notes.map((note, index) => (
-              <div
-                key={note._id}
-                className="note"
-                style={{ backgroundColor: note.color }}
-              >
-                {editMode[index] ? (
-                  <div>
-                    <input
-                      type="text"
-                      placeholder="New Title"
-                      value={form.title}
-                      onChange={(e) =>
-                        setForm({ ...form, title: e.target.value })
-                      }
-                    />
-                    <textarea
-                      placeholder="New Note"
-                      value={form.body}
-                      onChange={(e) =>
-                        setForm({ ...form, body: e.target.value })
-                      }
-                    />
-                    <button onClick={() => updateNote(index)}>Update</button>
-                  </div>
-                ) : (
-                  <div>
-                    <h3>{note.title}</h3>
-                    <p>{note.body}</p>
-                  </div>
-                )}
-                <button onClick={() => toggleEditMode(index)}>
-                  {editMode[index] ? "Cancel" : "Edit"}
-                </button>
-              </div>
-            ))}
-          </div>
+    <div className="notes-app">
+      <h2>Notes</h2>
+      <div className="notes-container">
+        <div className="new-note">
+          <h3>Create a New Note</h3>
+          <form onSubmit={addNote}>
+            <input
+              type="text"
+              placeholder="Title"
+              name="title"
+              value={form.title}
+              onChange={(e) => setForm({ ...form, title: e.target.value })}
+            />
+            <textarea
+              placeholder="Note"
+              name="body"
+              value={form.body}
+              onChange={(e) => setForm({ ...form, body: e.target.value })}
+            />
+            <input
+              type="color"
+              name="color"
+              value={form.color}
+              onChange={(e) => setForm({ ...form, color: e.target.value })}
+            />
+            <button type="submit">Add Note</button>
+          </form>
+        </div>
+        <div className="all-notes">
+          {notes.map((note, index) => (
+            <div
+              key={note._id}
+              className="note"
+              style={{ backgroundColor: note.color }}
+            >
+              {editMode[index] ? (
+                <div>
+                  <input
+                    type="text"
+                    placeholder="New Title"
+                    value={form.title}
+                    onChange={(e) =>
+                      setForm({ ...form, title: e.target.value })
+                    }
+                  />
+                  <textarea
+                    placeholder="New Note"
+                    value={form.body}
+                    onChange={(e) =>
+                      setForm({ ...form, body: e.target.value })
+                    }
+                  />
+                  <button onClick={() => updateNote(index)}>Update</button>
+                </div>
+              ) : (
+                <div>
+                  <h3>{note.title}</h3>
+                  <p>{note.body}</p>
+                </div>
+              )}
+              <button onClick={() => toggleEditMode(index)}>
+                {editMode[index] ? "Cancel" : "Edit"}
+              </button>
+            </div>
+          ))}
         </div>
       </div>
+    </div>
     </>
   );
 }
